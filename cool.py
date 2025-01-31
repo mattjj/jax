d3446b4402b0bb4395dfaa7353671d4f96f91aa9
Matthew Johnson <mattjj@google.com>
2025-01-31 05:08:36 +0000

    cool fact this was all dougal's idea...
    Co-authored-by: Dougal Maclaurin <dougalm@google.com>

--- cool.py
+++ cool.py
@@ -24,11 +24,11 @@ class CoolFun:
 
   def jvp(self, primals, tangents):
     # return jax.jvp(self.call, primals, tangents)
-    jaxpr = cool_module[self]
+    jaxpr = module.cool_module[self]
     jvp_prim = JVPOf(self)
-    if jvp_prim not in cool_module:
+    if jvp_prim not in module.cool_module:
       jvp_jaxpr, _ = ad.jvp_jaxpr(jaxpr, [True] * len(primals), True)
-      cool_module[jvp_prim] = jvp_jaxpr
+      module.cool_module[jvp_prim] = jvp_jaxpr
     out_primals_tangents = cool_p.bind(*primals, *tangents, prim=jvp_prim)
     return _split(out_primals_tangents)
 
@@ -36,24 +36,24 @@ def _split(lst):
   n = len(lst)
   return lst[:n//2], lst[n//2:]
 
-cool_module: dict[CoolFun, core.ClosedJaxpr] = {}
-
 cool_p = core.Primitive('cool')
 cool_p.multiple_results = True
 
 @cool_p.def_abstract_eval
-def _cool_abstract_eval(*args, prim):
-  return cool_module[prim].out_avals
+def _cool_abstract_eval(*in_avals, prim):
+  assert [*in_avals] == [*prim.in_avals]
+  return prim.out_avals
 
 @cool_p.def_impl
 def _cool_impl(*args, prim):
-  return core.jaxpr_as_fun(cool_module[prim])(*args)
+  return core.jaxpr_as_fun(module.cool_module[prim])(*args)
 
 def bind_cool_call(prim, *args):
-  if prim not in cool_module:
+  if prim not in module.cool_module:
+    module.cool_module[prim] = None
     jaxpr, out_avals, consts, () = pe.trace_to_jaxpr_dynamic(
         lu.wrap_init(prim.call), prim.in_avals)
-    cool_module[prim] = core.ClosedJaxpr(jaxpr, consts)
+    module.cool_module[prim] = core.ClosedJaxpr(jaxpr, consts)
   return cool_p.bind(*args, prim=prim)
 
 from jax._src.interpreters import ad
@@ -117,6 +117,21 @@ class CoolMul(CoolFun):
     zdot =  add(mul(x, ydot), mul(xdot, y))
     return [z], [zdot]
 
+def fact(n):
+  in_avals = out_avals = core.get_aval(n),
+  out, = bind_cool_call(CoolFact(in_avals), n)
+  return out
+
+@dataclass(frozen=True)
+class CoolFact(CoolFun):
+  in_avals: tuple[core.AbstractValue, ...]
+  def __str__(self): return 'Fact!'
+  def call(self, n):
+    return jax.lax.cond(n == 0, lambda: 1, lambda: n * fact(n - 1)),
+  @property
+  def out_avals(self):
+    return core.get_aval(1),
+
 #
 
 
@@ -130,19 +145,44 @@ x = y = jnp.ones(3)
 # print(jaxpr)
 # f(x, y)  # it runs!
 
-def g(x):
-  x = add(x, x)
-  x = add(x, x)
-  return mul(x, x)
 
-jaxpr = jax.make_jaxpr(g)(x)
-print(jaxpr)
+# def g(x):
+#   x = add(x, x)
+#   x = add(x, x)
+#   return mul(x, x)
 
-_, d = jax.jvp(g, (x,), (x,))
-print(d)
+# jaxpr = jax.make_jaxpr(g)(x)
+# print(jaxpr)
 
-jaxpr = jax.make_jaxpr(lambda x, y: jax.jvp(g, (x,), (x,)))(x, x)
-print(jaxpr)
+# _, d = jax.jvp(g, (x,), (x,))
+# print(d)
+
+# jaxpr = jax.make_jaxpr(lambda x, y: jax.jvp(g, (x,), (x,)))(x, x)
+# print(jaxpr)
+
+@dataclass(frozen=True)
+class Module:
+  cool_module: dict[CoolFun, core.ClosedJaxpr]
+  def __str__(self):
+    items = []
+    for prim, body in self.cool_module.items():
+      items.append(f'{prim} =\n{body}\n')
+    return '\n'.join(items)
+
+module = Module({})
+
+
+jaxpr = jax.make_jaxpr(fact)(5)
+print()
+print(module)
+
+from jax._src.lax.control_flow import conditionals
+@conditionals.cond_p.def_impl
+def cond_impl(idx, *args, branches):
+  return core.jaxpr_as_fun(branches[idx])(*args)
+
+print()
+print(fact(5))
 
 # print()
 # for k, v in cool_module.items():
